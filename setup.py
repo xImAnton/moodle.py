e6218886d0d2771af4dc2b97ebce1ea01a40eacb
xImAnton <anton.vogelsang@outlook.de>
2021-04-16 16:46:08 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_desc = f.read()
+
+
+setuptools.setup(
+    name="moodle-xImAnton_",
+    version="1.0.0",
+    author="xImAnton_",
+    description="A Python Wrapper for the Moodle Mobile API",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xImAnton/moodlecrawler",
+    project_urls={
+        "Bug Tracker": "https://github.com/xImAnton/moodlecrawler/issues"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8"
+)
